60c0494be40a8bf54f877dc097a3fc865251388e
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-06-24 02:21:18 +0530

    leetcode

--- searchingsorting/leetcode/solution.java
+++ searchingsorting/leetcode/solution.java
@@ -137,6 +137,52 @@ public class solution {
         return true;
     }
 
+    ////498. Diagonal Traverse
+
+    public int[] findDiagonalOrder(int[][] mat) {
+        if(mat.length == 0){
+            return new int[]{}; 
+        }
+        
+        int m = mat.length;
+        int n = mat[0].length;
+        
+        int row =0;
+        int col =0;
+        int res [] = new int[m*n];
+        
+        for(int i=0; i<res.length; i++){
+            res[i] = mat[row][col];
+            if((row + col) % 2 == 0){
+                if(col == n-1){
+                    row++;
+                }else if(row == 0){
+                    col++;
+                    
+                }else{
+                    row--;
+                    col++;
+                    
+                }
+                
+                
+            }else{
+                
+                if(row == m-1){
+                    col ++;
+                    
+                }else if(col == 0){
+                    row++;
+                }else{
+                    row++;
+                    col--;
+                }
+                
+            }
+        }
+        return res;
+    }
+
 
 
   public static void main(String[] args) {
