8b24839fc528200bfe05d012702041fb51730210
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-06-24 01:34:11 +0530

    leetcode

--- searchingsorting/leetcode/solution.java
+++ searchingsorting/leetcode/solution.java
@@ -102,6 +102,19 @@ public class solution {
    
     }
 
+    //   1486
+
+    public int xorOperation(int n, int start) {
+        
+        int ans = start;
+        
+        for(int i =1; i<n; i++){
+            ans^=(start+2*i);
+        }
+        return ans;
+        
+    }
+
 
 
 
