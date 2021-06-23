7f1cd07de8f28c99d2ecdbcdef756d5f02481caf
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-06-24 00:44:56 +0530

    leetcode

---
+++ searchingsorting/leetcode/solution.java
@@ -0,0 +1,45 @@
+public class solution {
+    
+
+
+    ///1380
+
+    public List<Integer> luckyNumbers (int[][] matrix) {
+        List<Integer> list = new ArrayList<>();
+        int n = matrix.length;
+        int m = matrix[0].length;
+        
+        
+        for(int i =0; i<n; i++){
+            int min = Integer.MAX_VALUE;
+            int idx = -1;
+              for(int j =0; j<m; j++){
+                  if(matrix[i][j] < min){
+                      min = matrix[i][j];
+                      idx =j;
+                      
+                  }
+              }
+            
+            int maxI = -1;
+            for(int j =0; j<n; j++){
+                if(matrix[j][idx] > maxI){
+                    maxI = matrix[j][idx];
+                    
+                }
+            }
+            
+            if(maxI == min){
+                list.add(maxI);
+            }
+        }
+        return list;
+    }
+
+
+
+
+  public static void main(String[] args) {
+        
+    }
+}
