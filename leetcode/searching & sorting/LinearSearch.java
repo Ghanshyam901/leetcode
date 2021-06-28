51622def6ea19dbde3b0f171d38e341449ec9738
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-06-29 00:40:13 +0530

    stack question

--- leetcode/searching & sorting/LinearSearch.java
+++ leetcode/searching & sorting/LinearSearch.java
@@ -16,6 +16,31 @@ public static boolean Linearsearch(int [] arr , int data){
 
 }
 
+/// 1021
+
+public String removeOuterParentheses(String s) {
+    Stack<Character> st = new Stack<>();
+    StringBuilder sb = new StringBuilder();
+    
+    for(int i =0; i<s.length(); i++){
+        char ch = s.charAt(i);
+        
+        if(ch == '('){
+            if(!st.isEmpty()){
+                sb.append(ch);
+            }
+            st.push(ch);
+        }else{
+            st.pop();
+            if(!st.isEmpty()){
+                sb.append(ch);
+            }
+        }
+    }
+    
+    return sb.toString();
+}
+
 
  public static void main(String[] args) {
         int [] arr ={2,4,8,1,5,0,88,98,66};
