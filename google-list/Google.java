1d57e8e2757955025657557ec8e4904e187bec85
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-06-30 17:23:22 +0530

    google-list

--- google-list/Google.java
+++ google-list/Google.java
@@ -65,6 +65,34 @@ public class google{
         return dp[0];
     }
 
+    // 41 find missiing number
+
+    public int firstMissingPositive(int[] nums) {
+     
+        int n = nums.length;
+        
+        for(int i =0; i<nums.length; i++){
+            if(i+1 == nums[i])continue;
+            
+            if(nums[i] < 1 || nums[i] > n)continue;
+            
+            int idx1 = i;
+            int idx2 = nums[i]-1;
+            
+            if(nums[idx2] == nums[idx1]) continue;
+            
+            int temp = nums[idx1];
+            nums[idx1] = nums[idx2];
+            nums[idx2] = temp;
+            i--;
+        }
+        int ans = -1;
+        for(int i =0; i<n; i++){
+            if(nums[i] != i+1) return i+1;
+        }
+        return n+1;
+    }
+
 
 public static void main(String[] args) {
     
