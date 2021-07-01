f2d1e8cc0c11a18f8057284cf7a3253fc04537bb
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-07-01 20:48:40 +0530

    merge two sorted list

--- linkedlist2/linkedList.java
+++ linkedlist2/linkedList.java
@@ -108,6 +108,32 @@ public static void fold(ListNode head) {
  
 }
 
+//// merge two sorted list
+
+public static ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    if(l1 == null ) return l2;
+    if(l2 == null) return l1;
+    
+    ListNode dummy  = new ListNode(-1);
+    ListNode prev = dummy;
+    
+    ListNode c1 = l1;
+    ListNode c2 = l2;
+    
+    while(c1 != null && c2 != null){
+        if(c1.val <= c2.val){
+            prev.next=c1;
+            c1 = c1.next;
+        }else{
+            prev.next = c2;
+            c2 = c2.next;
+        }
+        prev = prev.next;
+    }
+     prev.next = c1 != null ?c1:c2;
+     return dummy.next;
+ }
+
 
 
 
