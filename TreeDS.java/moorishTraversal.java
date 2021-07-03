5fb109a6b26b25b576b8569d2333ba00b064eac3
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-07-03 21:03:17 +0530

    moorishTraversal

---
+++ TreeDS.java/moorishTraversal.java
@@ -0,0 +1,56 @@
+
+import java.util.ArrayList;
+import java.util.LinkedList;
+
+public class moorishTraversal{
+
+    public static class TreeNode {
+        int val = 0;
+        TreeNode left = null;
+        TreeNode right = null;
+
+        TreeNode(int val) {
+            this.val = val;
+        }
+
+
+        public static TreeNode rightMostNode(TreeNode node, TreeNode curr){
+            while(node.right != null && node.right != curr){
+                    node  = node.right;
+            }
+            return node;
+        }
+
+        public static ArrayList<Integer> morrisInOrderTraversal(TreeNode root){
+                ArrayList<Integer> ans = new ArrayList<>();
+
+                TreeNode curr = root;
+
+                while(curr != null){
+                    TreeNode left = curr.left;
+                    if(left == null){
+                        ans.add(left.val);
+                        curr =curr.right;
+
+                    }else{
+                        TreeNode RightMostNode = rightMostNode(left,curr);
+                        if(RightMostNode.right == null){
+                            RightMostNode.right = curr;
+                            curr= curr.left;
+                        }else{
+                            RightMostNode.right = null;
+                            ans.add(curr.val);
+                            curr = curr.right;
+
+                        }
+                    }
+                }
+                return ans;
+        }
+
+
+
+    public static void main(String[] args) {
+        
+    }
+}
