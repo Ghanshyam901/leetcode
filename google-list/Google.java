313171baa5d2b1cf9e1084f9fefed9762688511f
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-07-14 02:39:22 +0530

    218. The Skyline Problem

--- google-list/Google.java
+++ google-list/Google.java
@@ -1,6 +1,10 @@
 import java.util.PriorityQueue;
 import java.util.HashMap;
 import java.util.Arrays;
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.List;
+import java.util.Collection;
 
 public class Google{
 
@@ -164,6 +168,43 @@ public class Google{
         }
         return queue.size()+1;
     }
+
+///218. The Skyline Problem
+
+
+        public List<List<Integer>> getSkyline(int[][] buildings) {
+            List<List<Integer>> result = new ArrayList<>();
+            List<int[]> height = new ArrayList<>();
+            for (int[] b : buildings) {
+                height.add(new int[]{b[0], -b[2]});
+                height.add(new int[]{b[1], b[2]});
+            }
+            Collections.sort(height, (a, b) -> {
+                if (a[0] != b[0])
+                    return a[0] - b[0];
+                return a[1] - b[1];
+            });
+            PriorityQueue<Integer> pq = new PriorityQueue<>((a, b) -> (b - a));
+            pq.offer(0);
+            int prev = 0;
+            for (int[] h : height) {
+                if (h[1] < 0) {
+                    pq.offer(-h[1]);
+                } else {
+                    pq.remove(h[1]);
+                }
+                int cur = pq.peek();
+                if (prev != cur) {
+                    List<Integer> tmp = new ArrayList<>();
+                    tmp.add(h[0]);
+                    tmp.add(cur);
+                    result.add(new ArrayList<>(tmp));
+                    prev = cur;
+                }
+            }
+            return result;
+        }
+    
 
 public static void main(String[] args) {
     
