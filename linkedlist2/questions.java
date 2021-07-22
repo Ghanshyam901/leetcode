a06446bbdc3b5170ac9c5f9620e46cef1ccfc638
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-07-22 19:34:27 +0530

    is cycle

--- linkedlist2/questions.java
+++ linkedlist2/questions.java
@@ -140,6 +140,27 @@ public class questions {
         
     }
 
+    // is cycle lc
+
+    public boolean hasCycle(ListNode head) {
+        if(head == null || head.next ==  null) return false;
+        
+        ListNode slow= head;
+        ListNode fast = head;
+        
+        
+        while(fast.next != null && fast.next.next != null){
+            fast = fast.next.next;
+            slow = slow.next;
+            
+            if(fast == slow){
+                return true;
+            }
+        }
+        
+        return false;
+    }
+
 
     public static void main(String[] args) {
         
