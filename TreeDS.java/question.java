858f327b8727a8f73d66c713d75cb92dca756fde
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-07-31 01:30:28 +0530

    revision

--- TreeDS.java/question.java
+++ TreeDS.java/question.java
@@ -73,6 +73,35 @@ public TreeNode deleteNode(TreeNode root, int key) {
     return root;
     
 }
+
+
+////671
+
+
+public int findSecondMinimumValue(TreeNode root) {
+    if(root == null || (root.left == null && root.right == null)){
+        return -1;
+    }
+    
+    int left = root.left.val;
+    int right = root.right.val;
+    
+    if(left == root.val){
+       left =  findSecondMinimumValue(root.left);
+    }
+    
+    if(right == root.val){
+        right = findSecondMinimumValue(root.right);
+    }
+    
+    if(left != -1 && right != -1){
+        return Math.min(left,right);
+    }else if(left != -1){
+        return left;
+    }else{
+        return right;
+    }
+}
     
     public static void main(String[] args) {
         
