c33ba2333b5a0f933971d22d402aab700b92d50a
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-08-08 22:23:20 +0530

    max product of three num

--- Arrays_String/Questions.java
+++ Arrays_String/Questions.java
@@ -253,7 +253,43 @@ public int nextGreaterElement(int n) {
             start ++;
         }
     }
+// ==================================================================
 
+// 628. Maximum Product of Three Numbers
+
+public int maximumProduct(int[] nums) {
+    int max1 = -(int)1e9;  
+    int max2 = -(int)1e9;
+    int max3 = -(int)1e9;
+      
+     int min1= (int)1e9;
+      int min2 = (int)1e9;
+      
+      for(int n=0;n<nums.length; n++){
+          if(nums[n] < min1){
+              min2= min1;
+              min1 = nums[n];
+          }else if(nums[n] < min2 ){
+              min2 = nums[n];
+          }
+          
+          if(nums[n] >= max1){
+              max3 = max2;
+              max2 = max1;
+              max1 = nums[n];
+              
+          }else if(nums[n] >= max2){
+              max3 = max2;
+              max2 = nums[n];
+              
+              
+          }else if(nums[n] >= max3){
+              max3 = nums[n];
+          }
+      }
+      
+      return Math.max(max1*max2*max3, max1*min1*min2);
+  }
 
 
 
