948cb77dece37d4aa605229822edc51c694f14b6
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-08-10 15:38:01 +0530

    fianl

--- Arrays_String/Questions.java
+++ Arrays_String/Questions.java
@@ -360,6 +360,56 @@ public int maxChunksToSorted(int[] arr) {
         return ans;
     }
 
+    // https://www.lintcode.com/problem/508/
+    // wiggle sort 508
+
+    public void wiggleSort(int[] arr) {
+        // write your code here
+    for(int i =0; i<arr.length-1; i++){
+        if(i %2 == 0){
+            if(arr[i+1] < arr[i]){
+                swap(arr, i, i+1);
+            }
+        }else{
+            if(arr[i+1] > arr[i]){
+                swap(arr,i,i+1);
+            }
+
+        }
+    }
+
+
+    }
+    public void swap(int [] arr , int si , int ei){
+        int temp = arr[si];
+        arr[si] = arr[ei];
+        arr[ei] = temp;
+    }
+
+    // =============================
+    // 747. Largest Number At Least Twice of Others
+    public int dominantIndex(int[] arr) {
+       
+        
+        int max = 0;
+        int secondMAX =0;
+        int maxIdx = -1;
+        
+        for(int i = 0; i<arr.length; i++){
+            if(arr[i] > max){
+                secondMAX = max;
+                max = arr[i];
+                maxIdx =i;
+            }else if(arr[i] > secondMAX){
+                secondMAX = arr[i];
+            }
+        }
+        
+        return secondMAX * 2 > max ? -1: maxIdx; 
+        
+        
+    }
+
 
 
 
