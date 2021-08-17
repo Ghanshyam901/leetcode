4fab5ff4100543638057668d0c434d6e3d8ce1ed
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-08-17 21:39:50 +0530

    final

--- Arrays_String/Questions.java
+++ Arrays_String/Questions.java
@@ -761,6 +761,30 @@ public int minDominoRotations(int[] tops, int[] bottoms) {
 }
 
 
+// 1030. Matrix Cells in Distance Order
+
+public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
+    int [][] arr = new int[rows*cols][2];
+    
+    for(int i =0; i<rows; i++){
+        for(int j =0; j<cols; j++){
+            int bno = i*cols+j;
+            arr[bno][0]= i;
+            arr[bno][1] = j;
+        }
+    }
+    Arrays.sort(arr,(a,b) ->{
+        
+        int d1 = Math.abs(a[0]-rCenter) + Math.abs( a[1]-cCenter);
+        int d2 = Math.abs(b[0]-rCenter)  + Math.abs(b[1]-cCenter);
+        
+        return d1-d2;
+    });
+    
+    return arr;
+}
+
+
 
 
 
