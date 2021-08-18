f5ff20f0c882ba83b43ccb071bbfbc40985b1a39
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-08-19 04:06:54 +0530

    graph

--- Graph/DFSquestion.java
+++ Graph/DFSquestion.java
@@ -108,6 +108,58 @@ public class DFSquestion {
     }
 
 
+    ///130. Surrounded Regions
+
+    public void solve(char[][] grid) {
+        int [][] dir = {{0,1},{0,-1},{1,0},{-1,0}};
+        int n = grid.length;
+        int m = grid[0].length;
+        
+        
+        for(int i =0; i<n; i++){
+            for(int j =0; j<m; j++){
+                
+                   if(i ==0 || j == 0 || i == n-1 || j == m-1 ){
+                    if(grid[i][j] == 'O'){
+                        DFS_REGION(grid,i,j,dir);
+                    }
+                    
+                }
+            }
+        }
+            
+            
+            for(int i=0; i<n; i++){
+                for(int j =0; j<m; j++){
+                    if(grid[i][j] == 'O'){
+                        grid[i][j] = 'X';
+                    }else if(grid[i][j] == '#'){
+                        grid[i][j] = 'O';
+                    }
+                }
+            }
+        }
+    
+    
+    
+    public void DFS_REGION(char[][] grid, int i, int j, int [][] dir){
+        
+        grid[i][j] ='#';
+           int n = grid.length;
+        int m = grid[0].length;
+        
+        for(int d = 0; d< dir.length; d++){
+            int r = i + dir[d][0];
+            int c = j + dir[d][1];
+            
+            if(r >= 0 && c >= 0 && r < n && c < m && grid[r][c] == 'O'){
+                DFS_REGION(grid,r,c,dir);
+            }
+        }
+        
+    }
+
+
     public static void main(String[] args) {
         
     }
