303d7d95dec88a6f171a3e453fc96949f6ed4b48
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-08-21 00:52:05 +0530

    multipl string

--- Arrays_String/Questions.java
+++ Arrays_String/Questions.java
@@ -783,6 +783,45 @@ public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
     
     return arr;
 }
+// 43. Multiply Strings
+    public String multiply(String num1, String num2) {
+        int [] ans = new int[num1.length() + num2.length()];
+        
+        for(int i = num1.length()-1; i>=0; i--){
+            for(int j = num2.length()-1; j>=0; j--){
+                
+                int idx1 = i+j;
+                int idx2 = i+j+1;
+                
+                int val = (num1.charAt(i)-'0') *(num2.charAt(j)-'0');
+                val = val + ans[idx2];
+            
+                int carry = val / 10;
+                
+                int rem = val %10;
+                
+                ans[idx2] = rem;
+                ans[idx1] +=carry;
+            }
+        }
+        
+        
+        StringBuilder sb = new StringBuilder();
+        
+        for(int i =0; i<ans.length; i++){
+            if(sb.length() == 0 && ans[i] == 0){
+                continue;
+            }
+            sb.append(ans[i]);
+        }
+        
+        if(sb.length() == 0){
+            return "0";
+        }
+        
+        return sb.toString();
+    }
+
 
 
 
