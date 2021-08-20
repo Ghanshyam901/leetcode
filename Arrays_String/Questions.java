cec130f0b2a9b94da2c9e705ef43793dee73d130
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-08-21 02:03:31 +0530

    max product

--- Arrays_String/Questions.java
+++ Arrays_String/Questions.java
@@ -888,6 +888,41 @@ public int[][] allCellsDistOrder(int rows, int cols, int rCenter, int cCenter) {
   }
 
 
+//   680. Valid Palindrome II
+public boolean validPalindrome(String s) {
+            
+    int i =0; 
+    int j = s.length()-1;
+       
+       while(i < j ){
+           if(s.charAt(i) !=  s.charAt(j)){
+               return isPalindrome(s,i+1,j) || isPalindrome(s,i,j-1);
+           }
+           i++;
+           j--;
+       }
+       return true;
+           
+   }
+   
+   private boolean isPalindrome(String s, int i, int j){
+       
+       while(i <  j){
+           if(s.charAt(i) != s.charAt(j)){
+              
+                return false;
+           
+           }
+           i++;
+           j--;
+          
+       }
+       return true;
+   }
+
+
+
+
 
 
 
