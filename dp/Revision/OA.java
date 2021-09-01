d20c07d6ba28332afea043a284a6b4a920343843
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-09-02 02:25:31 +0530

    dp

--- dp/Revision/OA.java
+++ dp/Revision/OA.java
@@ -138,6 +138,94 @@ public int maxSumIS(int arr[], int n)
         return count;
     }
 
+	// https://leetcode.com/problems/longest-palindromic-substring/submissions/
+
+	public String longestPalindrome(String s) {
+        boolean [][] dp = new boolean[s.length()][s.length()];
+        // int count =0;
+        String ans ="";
+        for(int gap =0; gap <s.length(); gap++){
+            for(int i=0,j = gap; j<dp.length; i++,j++){
+                if(gap == 0){
+                    dp[i][j] = true;
+                }else if(gap == 1){
+                    if(s.charAt(i) == s.charAt(j)){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }else{
+                    if(s.charAt(i) == s.charAt(j) && dp[i+1][j-1] == true){
+                        dp[i][j] = true;
+                    }else{
+                        dp[i][j] = false;
+                    }
+                }
+                
+                if(dp[i][j] == true){
+                   ans = s.substring(i,j+1);
+                }
+            }
+        }
+        return ans;
+    }
+//   64. Minimum Path Sum
+
+public int minPathSum(int[][] grid) {
+    int n = grid.length;
+    int m = grid[0].length;
+    
+    int[][] dp = new int[n][m];
+    int max = Integer.MIN_VALUE;
+    
+    for(int i = dp.length-1; i>=0; i--){
+        for(int j = dp[0].length-1; j>=0; j--){
+            
+            if(i == dp.length-1 && j == dp[0].length-1){
+             dp[i][j] = grid[i][j];
+                max = dp[i][j];
+            }else if(i == dp.length -1){
+                dp[i][j] = grid[i][j] + dp[i][j+1];
+                max = dp[i][j];
+            }else if(j == dp[0].length-1){
+                 dp[i][j] = grid[i][j] + dp[i+1][j];
+                max = dp[i][j];
+            }else{
+                
+                dp[i][j] = grid[i][j] + Math.min(dp[i+1][j],dp[i][j+1]);
+                max = dp[i][j];
+            }
+        }
+    }
+    return max;
+}
+
+// 45. Jump Game II
+
+public int jump(int[] nums) {
+        
+    Integer [] dp = new Integer[nums.length];
+    dp[nums.length-1] = 0;
+    
+    
+   
+    for(int i = nums.length-2; i>=0; i--){
+        int step = nums[i];
+        
+         int min = Integer.MAX_VALUE;
+        
+        for(int j =1; j<=step && i+j < nums.length; j++){
+            if(dp[i+j] != null && dp[i+j] < min){
+                min = dp[i+j];
+            }
+        }
+        
+        if(min != Integer.MAX_VALUE){
+            dp[i] = min+1;
+        }
+    }
+    return dp[0];
+}
 
     public static void main(String[] args) {
         
