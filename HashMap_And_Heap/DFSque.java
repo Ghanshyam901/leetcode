86c2bfff2bae13d0cb6cc5cfa9eda3315395ddda
Ghanshyam ram <Ghanshyam901@users.noreply.github.com>
2021-10-14 20:53:16 +0530

    dfs

--- HashMap_And_Heap/DFSque.java
+++ HashMap_And_Heap/DFSque.java
@@ -153,6 +153,54 @@ public int islandPerimeter(int[][] grid) {
         }
         
     }
+
+    // Number Of Distinct Island
+    public static void dfs(int [][] arr , int row, int col){
+        arr[row][col] = 0;
+        
+        if(row-1 >= 0 && arr[row-1][col] == 1){
+            psf.append("u");
+            dfs(arr,row-1,col);
+        }
+        
+        if(col +1 <arr[0].length && arr[row][col+1] ==1){
+            psf.append("r");
+            dfs(arr, row,col+1);
+        }
+        
+        if(row + 1 < arr.length && arr[row+1][col] == 1 ){
+            psf.append("d");
+            dfs(arr,row+1,col);
+        }
+        if(col - 1 >= 0 && arr[row][col-1] == 1){
+            psf.append("l");
+            dfs(arr,row,col-1);
+        }
+        psf.append("z");
+        
+        
+    }
+    public static StringBuilder psf = new StringBuilder();
+    
+    public static int numberofDistinctIslands(int[][] arr) {
+      //write your code here
+      
+      HashSet<String> set = new HashSet<>();
+      
+      
+      for(int i = 0; i< arr.length; i++){
+          for(int j =0; j<arr[0].length; j++){
+              if(arr[i][j] == 1){
+                  psf = new StringBuilder();
+                  psf.append("x");
+                  dfs(arr,i,j);
+                  set.add(psf.toString());
+                  
+              }
+          }
+      }
+      return set.size();
+    }
     public static void main(String[] args) {
         
     }
